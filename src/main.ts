f9e8d290591819779eaf591e36064a91f9e774b5
Muhammed Anshif <Muhammadanshif0@gmail.com>
2026-02-06 23:27:34 +0530

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import './css/style.css';
+import FullList from './model/FullList';
+import ListItem from './model/ListItem';
+import ListTemplate from './templates/ListTemplates';
+
+const initApp=():void =>{
+
+}
+
+document.addEventListener('DOMContentLoaded',initApp)
+
